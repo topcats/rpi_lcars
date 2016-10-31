d333c250ebf8e1fcbc8d1e3a618e1348dac07142
Topcat <coding.tris@the-owls.org>
2016-10-31 21:49:01 +0000

    merge.to.rpi_tc
    my merge before i properly branched

--- app/screens/main.py
+++ app/screens/main.py
@@ -9,34 +9,26 @@ from ui.widgets.lcars_widgets import *
 from ui.widgets.screen import LcarsScreen
 from ui.widgets.sprite import LcarsMoveToMouse
 
+from rescode import netfunc
+
 class ScreenMain(LcarsScreen):
     def setup(self, all_sprites):
-        all_sprites.add(LcarsBackgroundImage("assets/lcars_screen_1b.png"),
-                        layer=0)
+        all_sprites.add(LcarsBackgroundImage("assets/lcars_screen_1b.png"), layer=0)
         
         # panel text
-        all_sprites.add(LcarsText(colours.BLACK, (15, 44), "LCARS 105"),
-                        layer=1)
-        all_sprites.add(LcarsText(colours.ORANGE, (0, 135), "HOME AUTOMATION", 2),
-                        layer=1)
-        all_sprites.add(LcarsBlockMedium(colours.RED_BROWN, (145, 16), "LIGHTS"),
-                        layer=1)
-        all_sprites.add(LcarsBlockSmall(colours.ORANGE, (211, 16), "CAMERAS"),
-                        layer=1)
-        all_sprites.add(LcarsBlockLarge(colours.BEIGE, (249, 16), "ENERGY"),
-                        layer=1)
-        all_sprites.add(LcarsText(colours.BLACK, (444, 612), "192 168 0 3"),
-                        layer=1)
+        all_sprites.add(LcarsText(colours.BLACK, (15, 44), "LCARS 105"), layer=1)
+        all_sprites.add(LcarsText(colours.ORANGE, (0, 135), "HOME AUTOMATION", 2), layer=1)
+        all_sprites.add(LcarsBlockMedium(colours.RED_BROWN, (145, 16), "LIGHTS"), layer=1)
+        all_sprites.add(LcarsBlockSmall(colours.ORANGE, (211, 16), "CAMERAS"), layer=1)
+        all_sprites.add(LcarsBlockLarge(colours.BEIGE, (249, 16), "ENERGY"), layer=1)
+        #all_sprites.add(LcarsText(colours.BLACK, (444, 612), "192 168 0 3"), layer=1)
+        all_sprites.add(LcarsText(colours.BLACK, (444, 612), get_ip_address('eth0')), layer=1)
 
         # info text
-        all_sprites.add(LcarsText(colours.WHITE, (192, 174), "EVENT LOG:", 1.5),
-                        layer=3)
-        all_sprites.add(LcarsText(colours.BLUE, (244, 174), "2 ALARM ZONES TRIGGERED", 1.5),
-                        layer=3)
-        all_sprites.add(LcarsText(colours.BLUE, (286, 174), "14.3 kWh USED YESTERDAY", 1.5),
-                        layer=3)
-        all_sprites.add(LcarsText(colours.BLUE, (330, 174), "1.3 Tb DATA USED THIS MONTH", 1.5),
-                        layer=3)
+        all_sprites.add(LcarsText(colours.WHITE, (192, 174), "EVENT LOG:", 1.5), layer=3)
+        all_sprites.add(LcarsText(colours.BLUE, (244, 174), "2 ALARM ZONES TRIGGERED", 1.5), layer=3)
+        all_sprites.add(LcarsText(colours.BLUE, (286, 174), "14.3 kWh USED YESTERDAY", 1.5), layer=3)
+        all_sprites.add(LcarsText(colours.BLUE, (330, 174), "1.3 Tb DATA USED THIS MONTH", 1.5), layer=3)
         self.info_text = all_sprites.get_sprites_from_layer(3)
 
         # date display
@@ -44,17 +36,13 @@ class ScreenMain(LcarsScreen):
         self.lastClockUpdate = 0
         all_sprites.add(self.stardate, layer=1)
 
-        # buttons        
-        all_sprites.add(LcarsButton(colours.RED_BROWN, (6, 662), "LOGOUT", self.logoutHandler),
-                        layer=4)
-        all_sprites.add(LcarsButton(colours.BEIGE, (107, 127), "SENSORS", self.sensorsHandler),
-                        layer=4)
-        all_sprites.add(LcarsButton(colours.PURPLE, (107, 262), "GAUGES", self.gaugesHandler),
-                        layer=4)
-        all_sprites.add(LcarsButton(colours.PEACH, (107, 398), "WEATHER", self.weatherHandler),
-                        layer=4)
+        # buttons
+        all_sprites.add(LcarsButton(colours.RED_BROWN, (6, 662), "LOGOUT", self.logoutHandler), layer=4)
+        all_sprites.add(LcarsButton(colours.BEIGE, (107, 127), "SENSORS", self.sensorsHandler), layer=4)
+        all_sprites.add(LcarsButton(colours.PURPLE, (107, 262), "GAUGES", self.gaugesHandler), layer=4)
+        all_sprites.add(LcarsButton(colours.PEACH, (107, 398), "WEATHER", self.weatherHandler), layer=4)
 
-        # gadgets        
+        # gadgets
         all_sprites.add(LcarsGifImage("assets/gadgets/fwscan.gif", (277, 556), 100), layer=1)
         
         self.sensor_gadget = LcarsGifImage("assets/gadgets/lcars_anim2.gif", (235, 150), 100) 
@@ -112,5 +100,3 @@ class ScreenMain(LcarsScreen):
     def logoutHandler(self, item, event, clock):
         from screens.authorize import ScreenAuthorize
         self.loadScreen(ScreenAuthorize())
-    
-    
